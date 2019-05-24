02f6534990593c77aeab795a538ee47dfa435266
Lucas Javier Caron <31347680+lasffer@users.noreply.github.com>
2019-05-24 19:38:05 -0300

    Update ApliJavanation.java

--- Apli Javanation/src/apli/javanation/ApliJavanation.java
+++ Apli Javanation/src/apli/javanation/ApliJavanation.java
@@ -16,6 +16,7 @@ public class ApliJavanation {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
